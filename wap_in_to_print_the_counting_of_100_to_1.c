8067a31773a4ae4b2c266468b66a597a477ce8a2
kumaraditya62 <kumaraditya10vaishali@gmail.com>
2023-12-25 21:35:11 +0530

    adding new program

---
+++ wap_in_to_print_the_counting_of_100_to_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num=100;
+    while(num>=1)
+    {
+        printf("%d ",num);
+        num--;
+    }
+    printf("\n");
+    getch();
+}
