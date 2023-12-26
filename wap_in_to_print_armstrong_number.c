158c2a72fff7e50d8db028987ae5df3651c2636e
kumaraditya62 <kumaraditya10vaishali@gmail.com>
2023-12-26 19:06:24 +0530

    adding new program

---
+++ wap_in_to_print_armstrong_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num,mod,cube,sum=0,temp; 
+    printf("input a number:-");
+    scanf("%d",&num);
+    temp=num;
+    for(;num!=0;)
+    {
+    mod=num%10;
+    cube=mod*mod*mod;
+    sum=sum+cube;
+    num=num/10;
+    }
+    if(temp==sum)
+    {
+        printf("ARMSTRONG ");   
+    }
+    else
+    {
+        printf("NOT ");
+    }
+    getch(); 
+}
