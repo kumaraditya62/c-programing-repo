eb7193e1eb8a02f0c7ce2738c7e0d3f91371aeb1
kumaraditya62 <kumaraditya10vaishali@gmail.com>
2023-12-26 17:55:29 +0530

    adding new program

---
+++ wap_to_print_the_uppercase.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char i;
+    printf("UPPERCASE LETTERS:-\n");
+
+    for(i='A';i<='Z';i++)
+    {
+        printf("%c ",i);
+    }
+
+    printf("\n");
+
+    getch();
+}
