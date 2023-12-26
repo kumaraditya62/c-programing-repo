b495fe5e5f33375cd8d4c0efd410f29e802ba2cd
kumaraditya62 <kumaraditya10vaishali@gmail.com>
2023-12-26 18:12:01 +0530

    adding new program

---
+++ wap_in_c_to_print_the_lowercase.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char i;
+
+    printf("lowercase letter:- \n");
+
+    for(i='a';i<='z';i++)
+    {
+        printf("%c ",i);
+    }
+    printf("\n");
+
+    getch();
+}
